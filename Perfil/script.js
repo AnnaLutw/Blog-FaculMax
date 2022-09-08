ae165c4f83f8e1a11243a03d2fdabcfaa9bc2216
Ana luiza de sousa silva <correioaninha@gmail.com>
2022-09-08 09:33:16 -0300

    teste

---
+++ Perfil/script.js
@@ -0,0 +1,39 @@
+const mudaPag = document.querySelector('#margemLog');
+
+function verLogin(){
+    let user = JSON.parse(localStorage.getItem('usuario'));
+     
+     if(user){           
+         document.getElementById("margemLog").innerHTML = user.usuario;   
+         mudaPag.href = "../perfil/index.html";
+     }
+     
+     
+ }
+
+verLogin();
+   
+
+function verficaDados(){
+
+    
+    let user = JSON.parse(localStorage.getItem('usuario'));
+
+    document.getElementById("nomePerfil").innerHTML = user.usuario;
+    document.getElementById("loginSenha").innerHTML = user.senha;  
+    document.getElementById("email").innerHTML = user.email;  
+
+
+}
+
+function logout(){
+
+    confirm('Tem certeza?');
+    if(confirm){
+       
+    localStorage.removeItem('usuario');
+    window.location.assign('../paginaPrincipal/index.html');
+}}
+
+verficaDados();
+document.getElementById ('deslogar').addEventListener ('click', logout);
